1122348b6cbe51263c3a46a3496f5f69f5bc9413
D3TR0 <dominikkedzierski0607@gmail.com>
2023-01-09 19:41:53 +0100

    1

---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+
+def post_detail(request, post):
+    post=get_object_or_404(Post,slug=post,status='published')
+    return render(request, 'post_detail.html',{'post':post})
+
+
+def post_list(request):
+    posts = Post.published.all()
+
+    paginator = Paginator(posts, 10)  # 10 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'post_list.html', {'posts': posts, page: 'pages'})
