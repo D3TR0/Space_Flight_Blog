e7d7aece79c58711285aadcd6af27a3421564388
D3TR0 <dominikkedzierski0607@gmail.com>
2023-01-09 21:58:20 +0100

    działa logowanie i wylogowywanie oraz zakladanie konta bez potwierdzenia

--- Projekt_Blog_WWW/urls.py
+++ Projekt_Blog_WWW/urls.py
@@ -18,13 +18,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+import users
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
     path('', include('blog.urls', namespace='blog')),
     path('ckeditor/',include('ckeditor_uploader.urls')),
-    path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    #path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login', users.views.custom_login, name='login'),
+    path('logout', users.views.custom_logout, name='logout'),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth import get_user_model, login
+from django.contrib.auth import get_user_model, login, authenticate
 from .forms import UserRegistrationForm
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 
 def register(request):
     # Logged in user can't register a new account
@@ -25,3 +29,38 @@ def register(request):
         template_name = "register.html",
         context={"form":form}
         )
+
+@login_required
+def custom_logout(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("/")
+
+
+def custom_login(request):
+    if request.user.is_authenticated:
+        return redirect('homepage')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Hello <b>{user.username}</b>! You have been logged in")
+                return redirect('http://127.0.0.1:8000/')
+
+        else:
+            for error in list(form.errors.values()):
+                messages.error(request, error)
+
+    form = AuthenticationForm()
+
+    return render(
+        request=request,
+        template_name="login.html",
+        context={'form': form}
+    )
